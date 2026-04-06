397fc1d59fd6b8b6203509f091d2450da7a35ae7
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2026-04-06 22:19:22 +0100

    Simple movement and boulder pushing

--- docs/js/bolder.js
+++ docs/js/bolder.js
@@ -1,5 +1,13 @@
 const bolder = {
 	go: () => {
 		map.loadMap(0)
+
+		document.addEventListener("keydown", (event) => {
+			const keyName = event.key;
+			if ( keyName === "ArrowUp" || keyName === 'k' ) { map.bobUp() }
+			if ( keyName === "ArrowDown" || keyName === 'm'  ) { map.bobDown() }
+			if ( keyName === "ArrowLeft" || keyName === 'z'  ) { map.bobLeft() }
+			if ( keyName === "ArrowRight"  || keyName === 'x' ) { map.bobRight() }
+		} )
 	}
 }
--- docs/js/map.js
+++ docs/js/map.js
@@ -1,6 +1,17 @@
 const map = {
 	level: 0,
-	entities: [],
+	diamonds: 0,
+	boulders: [],
+	bob: {},
+
+	grid: [],
+	gridtype: {
+		EMPTY: ' ',
+		WALL: '#',
+		EARTH: '.',
+		BOULDER: 'O',
+		DIAMOND: '^'
+	},
 
 	/**
 	 * loads the numbered level 
@@ -14,31 +25,150 @@ const map = {
 		let view = document.getElementById( "-viewport" )
 
 		for ( let y = 0; y < lobj.height; y++ ) {
+			map.grid[y] = []
+
 			for ( let x = 0; x < lobj.height; x++ ) {
 				let elem = document.createElement( 'div' )
 				elem.style.left = x*64 + 'px'
 				elem.style.top = y*64 + 'px'
+				elem.setAttribute( 'id', 'entity'+c )
 				view.appendChild( elem )
 
+				let loc = { type: lobj.map[c], id:'entity'+c, elem:elem }
+				map.grid[y][x] = loc
+
 				switch( lobj.map[c] ) {
-					case '#':
+					case map.gridtype.WALL:
 						elem.setAttribute( 'class', 'wall entity' )
 						break
-					case '.':
+					case map.gridtype.EARTH:
 						elem.setAttribute( 'class', 'earth entity' )
 						break
-					case 'O':
+					case map.gridtype.BOULDER:
 						elem.setAttribute( 'class', 'boulder entity' )
+						map.boulders.push( { x:x,y:y, id:'entity'+c } )
 						break
-					case '^':
+					case map.gridtype.DIAMOND:
 						elem.setAttribute( 'class', 'diamond entity' )
+						map.diamonds += 1
 						break
+					
+					// 'x' is Bob, so set him up.
 					case 'x':
-						elem.setAttribute( 'class', 'bob entity' )
+						loc.type = map.gridtype.EMPTY
+						elem.setAttribute( 'class', 'entity' )
+						map.bob = { x:x, y:y, id:'bob' }
 						break
 				}
 				c=c+1
 			}
 		}
+		map.moveBob()
+	},
+
+	/**
+	 * Move bob up, if bob will move up.
+	 */
+	bobUp: () => {
+		let next = map.grid[map.bob.y-1][map.bob.x].type
+		if ( next === map.gridtype.WALL || next === map.gridtype.BOULDER ) {
+			return
+		}
+		map.bob.y -= 1
+		map.emptyLoc( map.bob )
+		map.moveBob()
+	},
+
+	/**
+	 * Move bob up, if bob will move up.
+	 */
+	bobLeft: () => {
+		let next = map.grid[map.bob.y][map.bob.x-1].type
+		if ( next === map.gridtype.WALL ) {
+			return
+		}
+
+		// Pushing a boulder depends on what's after
+		if ( next === map.gridtype.BOULDER ) {
+			let after = map.grid[map.bob.y][map.bob.x-2].type
+			if ( after === map.gridtype.EMPTY ) {
+				map.moveBoulderLeft({y:map.bob.y,x:map.bob.x-1})
+			} else {
+				return
+			}
+		}
+
+		map.bob.x -= 1
+		map.emptyLoc( map.bob )
+		map.moveBob()
+	},
+
+	/**
+	 * Move bob up, if bob will move up.
+	 */
+	bobRight: () => {
+		// Can't walk through walls.
+		let next = map.grid[map.bob.y][map.bob.x+1].type
+		if ( next === map.gridtype.WALL ) {
+			return
+		}
+
+		// Pushing a boulder depends on what's after
+		if ( next === map.gridtype.BOULDER ) {
+			let after = map.grid[map.bob.y][map.bob.x+2].type
+			if ( after === map.gridtype.EMPTY ) {
+				map.moveBoulderRight({y:map.bob.y,x:map.bob.x+1})
+			} else {
+				return
+			}
+		}
+
+		map.bob.x += 1
+		map.emptyLoc( map.bob )
+		map.moveBob()
+	},
+
+	/**
+	 * Move bob up, if bob will move up.
+	 */
+	bobDown: () => {
+		let next = map.grid[map.bob.y+1][map.bob.x].type
+		if ( next === map.gridtype.WALL || next === map.gridtype.BOULDER ) {
+			return
+		}
+		map.bob.y += 1
+		map.emptyLoc( map.bob )
+		map.moveBob()
+	},
+
+	moveBob: () => {
+		// Move bob's sprite
+		let elem = document.getElementById( map.bob.id )
+		elem.style.left = map.bob.x*64 + 'px'
+		elem.style.top = map.bob.y*64 + 'px'
+	},
+
+	emptyLoc: ( loc ) => {
+		let entity = map.grid[loc.y][loc.x]
+		entity.elem.setAttribute( 'class', 'entity' )
+		entity.type = map.gridtype.EMPTY
+	},
+
+	moveBoulderRight: ( loc ) => {
+		map.emptyLoc( loc )
+		loc.x += 1
+		map.boulderLoc( loc )
+	},
+
+	moveBoulderLeft: ( loc ) => {
+		map.emptyLoc( loc )
+		loc.x -= 1
+		map.boulderLoc( loc )
+	},
+
+	boulderLoc: ( loc ) => {
+		let entity = map.grid[loc.y][loc.x]
+		entity.elem.setAttribute( 'class', 'entity boulder' )
+		entity.type = map.gridtype.BOULDER
 	}
 }
